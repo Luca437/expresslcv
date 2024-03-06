240f3760e93b9a8cb5bee40051f9d88e017a1a0d
Luca437 <luca.carnevale647@gmail.com>
2024-03-06 12:02:57 +0100

    change port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 5001);
 
 console.log("+++++++++++++++"+ app.get('port'));
 
